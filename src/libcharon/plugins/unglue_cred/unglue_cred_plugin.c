0370cdbb04fd65542c2222ea864388235a51cff7
Evgeny Kolesnikov <evgenyz@gmail.com>
2017-05-08 03:06:00 +0700

    Added HMAC secret type

--- src/libcharon/plugins/unglue_cred/unglue_cred_plugin.c
+++ src/libcharon/plugins/unglue_cred/unglue_cred_plugin.c
@@ -11,12 +11,16 @@ typedef struct private_unglue_cred_plugin_t private_unglue_cred_plugin_t;
 struct private_unglue_cred_plugin_t {
 	unglue_cred_plugin_t public;
 	callback_cred_t      *cb;
+	signer_t             *signer;
+	char*                keys[10];
+	bool                 enable_hmac;
+	bool                 enable_plain;
 };
 
 METHOD(plugin_t, get_name, char*,
 	private_unglue_cred_plugin_t *this)
 {
-	return "unglue_cred";
+	return "unglue-cred";
 }
 
 static shared_key_t* callback_shared(private_unglue_cred_plugin_t *this,
@@ -27,7 +31,8 @@ static shared_key_t* callback_shared(private_unglue_cred_plugin_t *this,
 	shared_key_t *shared;
 	char id[512] = {0};
 	char pw[512] = {0};
-	char *dev, *acc;
+	uint dev, acc;
+	uint key_no;
 	
 	DBG2(DBG_CFG, "unglue cred for id: '%Y' (type: %d)", other, type);
 	
@@ -50,22 +55,54 @@ static shared_key_t* callback_shared(private_unglue_cred_plugin_t *this,
 	{
 		*match_other = ID_MATCH_PERFECT;
 	}
-	
+
 	snprintf(id, sizeof(id), "%Y", other);
-	if (sscanf(id, "%m[0-9]@%m[0-9].device", &dev, &acc) != 2)
+
+	if (this->enable_hmac && sscanf(id, "%u@%u.hmac-S%u", &dev, &acc, &key_no) == 3)
 	{
-		DBG1(DBG_CFG, "unable to parse the id");
-		return NULL;
+		DBG2(DBG_CFG, "unglue-cred type = hmac");
+
+		snprintf(pw, sizeof(pw), "%u.%u", acc, dev);
+		DBG2(DBG_CFG, "secret plain: %s, key#: %u", pw, key_no);
+
+		if (this->keys[key_no] == NULL)
+		{
+			DBG1(DBG_CFG, "requested key #%u is NULL, unable to sign", key_no);
+			return NULL;
+		}
+
+		char sig_hex[41] = {0};
+		uint8_t sig[20] = {0};
+		if (!this->signer->set_key(this->signer, chunk_from_str(this->keys[key_no])))
+		{
+			DBG1(DBG_CFG, "unable to set key #%u", key_no);
+			return NULL;
+		}
+		if (!this->signer->get_signature(this->signer, chunk_from_str(pw), sig))
+		{
+			DBG1(DBG_CFG, "unable to sign with key #%u", key_no);
+			return NULL;
+		}
+		chunk_to_hex(chunk_from_thing(sig), sig_hex, FALSE);
+		DBG2(DBG_CFG, "secret hmac: %s", sig_hex);
+
+		shared = shared_key_create(type, chunk_clone(chunk_from_str(sig_hex)));
+		return shared->get_ref(shared);
 	}
-	
-	snprintf(pw, sizeof(pw), "%s.%s@unglue", acc, dev);
-	free(dev);
-	free(acc);
-	
-	DBG2(DBG_CFG, "secret: %s", pw);
-	shared = shared_key_create(type, chunk_clone(chunk_from_str(pw)));
-	
-	return shared->get_ref(shared);
+
+	if (this->enable_plain && sscanf(id, "%d@%d.device", &dev, &acc) == 2)
+	{
+		DBG2(DBG_CFG, "unglue-cred type = plain");
+
+		snprintf(pw, sizeof(pw), "%u.%u@unglue", acc, dev);
+		DBG2(DBG_CFG, "secret plain: %s", pw);
+
+		shared = shared_key_create(type, chunk_clone(chunk_from_str(pw)));
+		return shared->get_ref(shared);
+	}
+
+	DBG1(DBG_CFG, "unable to parse the id, unknown or disabled secret type");
+	return NULL;
 }
 
 static bool plugin_cb(private_unglue_cred_plugin_t *this,
@@ -75,10 +112,18 @@ static bool plugin_cb(private_unglue_cred_plugin_t *this,
 	{
 		
 		lib->credmgr->add_set(lib->credmgr, &this->cb->set);
+		this->signer = lib->crypto->create_signer(lib->crypto, AUTH_HMAC_SHA1_160);
+
+		if (!this->signer)
+		{
+			DBG1(DBG_LIB, "unable to create AUTH_HMAC_SHA1_160 signer");
+			return FALSE;
+		}
 	}
 	else
 	{
 		lib->credmgr->remove_set(lib->credmgr, &this->cb->set);
+		if (this->signer) (this->signer)->destroy(this->signer);
 	}
 	return TRUE;
 }
@@ -88,7 +133,9 @@ METHOD(plugin_t, get_features, int,
 {
 	static plugin_feature_t f[] = {
 		PLUGIN_CALLBACK((plugin_feature_callback_t)plugin_cb, NULL),
-			PLUGIN_PROVIDE(CUSTOM, "unglue"),
+			PLUGIN_PROVIDE(CUSTOM, "unglue-cred"),
+				PLUGIN_DEPENDS(HASHER, HASH_SHA1),
+				PLUGIN_DEPENDS(SIGNER, AUTH_HMAC_SHA1_160),
 	};
 	*features = f;
 	return countof(f);
@@ -122,6 +169,19 @@ plugin_t *unglue_cred_plugin_create()
 		},
 		.cb = callback_cred_create_shared((void*)callback_shared, this),
 	);
+
+	this->enable_hmac = lib->settings->get_bool(lib->settings, "%s.plugins.unglue-cred.hmac", TRUE, lib->ns);
+	this->enable_plain = lib->settings->get_bool(lib->settings, "%s.plugins.unglue-cred.plain", FALSE, lib->ns);
+
+	DBG1(DBG_CFG, "unglue-cred: hmac=%s, plain=%s", this->enable_hmac ? "yes" : "no", this->enable_plain ? "yes" : "no");
+
+	this->keys[0] = lib->settings->get_str(lib->settings, "%s.plugins.unglue-cred.key0", NULL, lib->ns);
+	this->keys[1] = lib->settings->get_str(lib->settings, "%s.plugins.unglue-cred.key1", NULL, lib->ns);
+	this->keys[2] = lib->settings->get_str(lib->settings, "%s.plugins.unglue-cred.key2", NULL, lib->ns);
+	this->keys[3] = lib->settings->get_str(lib->settings, "%s.plugins.unglue-cred.key3", NULL, lib->ns);
+	for (uint i = 0; i < 10; i++) {
+		if (this->keys[i] != NULL) DBG1(DBG_CFG, "loaded unglue-cred key #%u", i);
+	}
 
 	return &this->public.plugin;
 }
